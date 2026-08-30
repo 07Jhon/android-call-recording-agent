5fefae152093442f18c86e9b18aea75d73d43542
JohnFanilo <andriamparanyfanilo@gmail.com>
2026-08-30 08:02:10 +0300

    file pp

--- backend/src/main/java/com/enterprise/callrecorder/service/CallRecordingService.java
+++ backend/src/main/java/com/enterprise/callrecorder/service/CallRecordingService.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class RegisteredDevice {
+public class CallRecordingService {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
